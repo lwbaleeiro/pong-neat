06a7244741a4591d45863c393c485d4993158408
lwbaleeiro <dev.baleeiro@gmail.com>
2024-05-19 01:18:54 -0300

    pong game initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from pong import Game
+
+class PongGame:
+    def __init__(self,window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
+
+    def test_ai(self):
+
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                self.game.move_paddle(left=True, up=False)
+
+            self.game.loop()
+            self.game.draw()
+            pygame.display.update()
+
+        pygame.quit()
